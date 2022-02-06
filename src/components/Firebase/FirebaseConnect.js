783ef2d822ad2d215137e198fad5db8458296190
unknown <mdmehedyh55@gamil.com>
2022-02-06 20:01:23 +0600

    install done

---
+++ src/components/Firebase/FirebaseConnect.js
@@ -0,0 +1,17 @@
+// import { initializeApp } from 'firebase/app';
+// import { firebaseConfig } from './firebase.Config';
+// import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
+// initializeApp(firebaseConfig);
+
+// export const googleSignupHandler = () => {
+//     const provider = new GoogleAuthProvider();
+//     const auth = getAuth();
+//     signInWithPopup(auth, provider)
+//         .then((res) => {
+//             console.log(res);
+//         }).catch((err) => {
+//             console.log(err);
+//         });
+// }
+
